255e2f8cfd6c446123687ed9563ccf11c4c7369a
yeonmi park <pyeonm25@gmail.com>
2025-01-15 17:35:28 +0900

    feat : add controller package

--- level9/src/main/_Main.java
+++ level9/src/main/_Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import controller.Controller;
+
 public class _Main {
 	public static void main(String[] args) {
 		
-		System.out.println("메인");
+		Controller con = new Controller();
+		
 	}
 }
